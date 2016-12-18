7555ed0b6cee518771dc9767e5c0bab44dcb02fd
Miki Morisawa <mikimorisawa77@gmail.com>
2016-12-18 12:51:20 +0000

    user prorile

--- db/migrate/20161211060833_create_users.rb
+++ db/migrate/20161211060833_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
+      t.string :location
+      t.text :about
 
       t.timestamps null: false
 
